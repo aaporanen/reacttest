83028520c6c41248fd9f380f28b25a1bd2ca86ff
Aapeli Poranen <aapeli.poranen@gmail.com>
2022-04-16 15:33:11 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { AppBar, Button, IconButton, Toolbar, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import useSWR from 'swr';
+
+const fetcher = (url: string) => fetch(url).then(response => response.json());
 
 function App() {
+  const { data, error } = useSWR("https://net6testapp.azurewebsites.net/test", fetcher);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <FontAwesomeIcon icon={faBars} />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Test
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      {data}
+    </Box>
   );
 }
 
